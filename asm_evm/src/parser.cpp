2537b87171333342b3b5374208cc72cfb48aa8c2
newenclave <pikrat.acid@gmail.com>
2018-03-21 11:15:54 +0200

    assembler. parser

--- asm_evm/src/parser.cpp
+++ asm_evm/src/parser.cpp
@@ -1,2 +1,18 @@
 #include "parser.h"
 
+namespace evm { namespace assembler {
+	parser::parser(lexer lex)
+		:lexer_(std::move(lex))
+	{}
+
+	void parser::reset()
+	{
+		lexer_.reset();
+	}
+
+	lexer &parser::get_lexer()
+	{
+		return lexer_;
+	}
+
+}}
--- asm_evm/src/parser.h
+++ asm_evm/src/parser.h
@@ -1,7 +1,17 @@
 #pragma once 
 
+#include <vector>
+#include "lexer.h"
+
 namespace evm { namespace assembler {
 	class parser {
+	public:
+		parser(lexer lex);
+		void reset();
+		lexer &get_lexer();
 
+	private:
+		lexer lexer_;
+		std::vector<std::uint8_t> mem_;
 	};
 }}
